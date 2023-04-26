dcc17c43e25a011c1ce123420e72ab271037ddc7
Mike Ciffone <75536523+mikeciffone@users.noreply.github.com>
2023-04-26 12:27:58 -0500

    Create convert-prices.js

---
+++ convert-prices.js
@@ -0,0 +1,16 @@
+// convert-prices.js
+
+$(function() {
+  // Click event handler for the toggle switch
+  $('#toggle-currency').on('click', function() {
+    // Get the current currency and toggle it
+    var currency = $(this).prop('checked') ? 'USD' : 'GBP';
+    // Make an AJAX request to the PHP endpoint to get the converted prices
+    $.getJSON('/convert-prices.php', { currency: currency }, function(data) {
+      // Update the prices on the page with the converted prices
+      $('#plan1-price').text(data.plan1);
+      $('#plan2-price').text(data.plan2);
+      $('#plan3-price').text(data.plan3);
+    });
+  });
+});
